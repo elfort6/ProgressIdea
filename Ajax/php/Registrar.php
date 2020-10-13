4384b10de7b2ce1de88302c8173e5e5e14436ee0
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2020-10-13 13:52:35 -0600

    Actualizacion
    pusheando
    
    Co-Authored-By: elfort6 <36178332+elfort6@users.noreply.github.com>

--- Ajax/php/Registrar.php
+++ Ajax/php/Registrar.php
@@ -6,8 +6,23 @@ include 'conexion.php';
     $usuario=mysqli_real_escape_string($conexion, $_POST['usuario']);
     $contraseña=password_hash($_POST['pswd'], PASSWORD_DEFAULT);
     $idRandom = rand(0,100000);
-    $insertTipoDeUsuario=mysqli_query($conexion, 'insert into tipodeusuario(tipoUsuario) values('.$idRandom.','.$_POST['userType'].')');
-   // ("'.$usuario.'","'.$contraseña.'","'.$contraseña.'")') or die('<p>Error al registrar</p><br>'.mysqli_error($conexion));
+    //$correo=mysqli_real_escape_string($conexion, $_POST['correo']);
+    
+    $insertTipoDeUsuario=mysqli_query($conexion, 'insert into tipodeusuario(idTipoDeUsuario, tipoUsuario) values('.$idRandom.','.$_POST['userType'].')')
+    or die('<p>Error al registrar</p><br>'.mysqli_error($conexion));
+
+    //$insertCorreo=mysqli_query($conexion, 'insert into correo(idCorreo, correo) values('.$idRandom.',yo@gmail.com)')
+   // or die('<p>Error al registrar</p><br>'.mysqli_error($conexion));
+
+    $insertTelefono=mysqli_query($conexion, 'insert into telefono(idTelefono, numeroTelefono) values('.$idRandom.','.$_POST['telefono'].')')
+    or die('<p>Error al registrar</p><br>'.mysqli_error($conexion));
+
+    $insertTelefono=mysqli_query($conexion, 'insert into usuario(idTelefono, numeroTelefono) values('.$idRandom.','.$_POST['telefono'].')')
+    or die('<p>Error al registrar</p><br>'.mysqli_error($conexion));
+
+    //$insertTelefono=mysqli_query($conexion, 'insert into telefono(idTelefono, numeroTelefono) values('.$idRandom.','.$_POST['telefono'].')')
+    //or die('<p>Error al registrar</p><br>'.mysqli_error($conexion));
+    // ("'.$usuario.'","'.$contraseña.'","'.$contraseña.'")') or die('<p>Error al registrar</p><br>'.mysqli_error($conexion));
     //redirección
   //  header('location: ./');
 
