73d93b31fc319ee3de13368e72bcc10ae51cf9ab
elfort6 <36178332+elfort6@users.noreply.github.com>
2020-10-13 10:46:59 -0600

    Conexion
    Se configuro la conexion a la base de datos

--- Ajax/php/conexion.php
+++ Ajax/php/conexion.php
@@ -1,3 +1,3 @@
 <?php
-$conexion=mysqli_connect('localhost', 'root', '', 'dbregistro') or die('Error al conectar con la base de datos');
+$conexion=mysqli_connect('localhost', 'root', '', 'Progressidea') or die('Error al conectar con la base de datos');
 ?>
