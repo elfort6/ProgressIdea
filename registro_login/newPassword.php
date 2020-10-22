981b717c7d7247a46c8e256c48b788b7efdf1f99
elfort6 <36178332+elfort6@users.noreply.github.com>
2020-10-22 08:52:01 -0600

    solucionado verifi
    se soluciono la verificacion del id que se resibe para recuperar cuentas

--- registro_login/newPassword.php
+++ registro_login/newPassword.php
@@ -2,11 +2,10 @@
 require "../Ajax/php/conexion.php";
 
 $consultRecu = mysqli_query($conexion,'SELECT * FROM recuperacioncuenta WHERE idRecuperacion = "'.$_GET['idpwd'].'"');
-if($consultRecu==null){
-    header ('location: ../tinks.html');
-
-}else{
-    $consulta=$consultRecu->fetch_assoc();
+$consulta=$consultRecu->fetch_assoc();
+if(isset($consulta['idUsuario'])){
+    
+    
 
 
 ?>
@@ -39,65 +38,59 @@ if($consultRecu==null){
         <div class="panel-heading panel-primary">
             <h2 class="header panel-primary">Cambio de contraseña</h2>
         </div>
-        <!-- /.panel-body -->
         
+        <!-- /.panel-body -->
         <div class="panel-body col-lg-12 pt-4 pb-5">
 
-            <div class="col-lg-12" id="form">
-                <div class="form-group col-lg-12">
-                    <label>Nueva Contraseña</label>
-                    <input class="form-control" type="password" id="pswd1" name="pswd1" required=" ">
-                    <div class="valid-feedback">¡Ok válido!</div>
-                    <div class="invalid-feedback">No Valido.</div>
-                </div>
-                <div class="form-group col-lg-12">
-                    <label>Confirmar contraseña</label>
-                    <input class="form-control" type="password" id="pswd2" name="pswd2" required=" ">
-                    <div class="valid-feedback">¡Ok válido!</div>
-                    <div class="invalid-feedback">No Valido.</div>
-                </div>
-                
-                <div class="row">
-                    <div class="form-group col-lg-6 col-md-6 mt-lg-5">
-                        <a class="btn-lg btn-block col-lg-12 btn-danger text-center text-white" style="border-bottom: solid #69121A 2px; border-right: solid #69121A 2px; text-decoration: none;" id="btnCancelar" href="login.html">Cancelar</a>
-                    </div>
+            <div class="col-lg-12">
+                <form role="form" action="Ajax/php/IniciarSesion.php" method="POST" class="needs-validation" novalidate>
+                    <div>
+                        <div class="form-group col-lg-12">
+                            <label>Nueva Contraseña</label>
+                            <input class="form-control" type="password" id="pswd1" name="pswd1" required=" ">
+                            <div class="valid-feedback">¡Ok válido!</div>
+                            <div class="invalid-feedback">Complete el campo.</div>
+                        </div>
+                        <div class="form-group col-lg-12">
+                            <label>Confirmar contraseña</label>
+                            <input class="form-control" type="password" id="pswd2" name="pswd2" required=" ">
+                            <div class="valid-feedback">¡Ok válido!</div>
+                            <div class="invalid-feedback">Complete el campo.</div>
+                        </div>
+                        
+                        <div class="row">
+	                        <div class="form-group col-lg-6 col-md-6 mt-lg-5">
+                                <a class="btn-lg btn-block col-lg-12 btn-danger text-center text-white" style="border-bottom: solid #69121A 2px; border-right: solid #69121A 2px; text-decoration: none;" id="btnCancelar" href="login.html">Cancelar</a>
+                            </div>
 
-                    <div class="form-group col-lg-6 col-md-6 mt-lg-5">
-                        <button type="submit" class="btn-lg btn-block col-lg-12 btn-success" id="btnAcpetar">Aceptar</button>
+	                        <div class="form-group col-lg-6 col-md-6 mt-lg-5">
+	                            <button type="submit" class="btn-lg btn-block col-lg-12 btn-success" id="btnAcpetar">Aceptar</button>
+	                        </div>
+                        </div>
+
+
                     </div>
-                </div>
+                    <!-- /.row -->
+
+                </form>
 
             </div>
             <!-- /.col-lg-12 -->
+            <!-- /.col-lg-12 -->
+<!-- /.col-lg-12 -->
         </div>
         <!-- /.panel-body -->
     </div>
     <!-- /.container-->
     
     <!-- jQuery -->
-    <script src="../librerias/jQuery/js/jQuery.min.js"></script>
+    <script src="../librerias/jQuery/js/jQuery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="../librerias/bootstrap/js/bootstrap.min.js"></script>
 
     <!-- JavaScript -->
-    <script src="../js/validar.js"></script>
-    <script type="text/javascript">
-        function enviar(){
-            var valor = validar();
-            if (valor){
-                var usuario = $("#usuario").val(), clave = $("#clave").val();
-                $.ajax({
-                  method: "POST",
-                  url: "../Ajax/php/IniciarSesion.php",
-                  data: {"usuario":usuario,"clave":clave}
-                }).done(function( msg ) {
-                    alert( "Data Saved: " + msg );
-                  });
-
-            }
-        }
-    </script>
+    <script src="../js/codigo2.js"></script>
 </body>
 
 </html>
@@ -105,7 +98,7 @@ if($consultRecu==null){
 <?php 
 
 
+}else{
+        header ('location: ../tinks.html');
 }
 ?>
-
-
