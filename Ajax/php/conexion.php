11bfa38385bdfc92e150cb41c0040d79579aaeae
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2020-10-21 23:59:11 -0600

    se eliminaron archivos antiguos

--- Ajax/php/conexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conexion=mysqli_connect('localhost', 'root', '', 'Progressidea') or die('Error al conectar con la base de datos');
-?>
--- Sources/json.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$data = file_get_contents("PaisesYprefijos.json");
-	$datos = json_decode($data, true);
-	$opciones = "";
-	foreach ($datos as $pais => $prefijo){
-        $opciones .= '<option value="'."{$prefijo}".'"'.">{$pais}</option>";
-    }
-    echo $opciones;
- ?>
