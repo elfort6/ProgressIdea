21be5bcaec7a267f0e8c1e2679d092a76f531577
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2020-11-22 17:14:55 -0600

    Nueva version.
    Existe el detalle de la falta de imagenes en los proyectos del landing.

--- Index.php
+++ Index.php
@@ -33,7 +33,6 @@
     </nav>
     <!--Final del Nav-->
     <div class="container">
-
         <div class="nav-scroller py-1 mb-2 ">
             <nav class="nav d-flex justify-content-between">
                 <a class="p-2 text-muted" href="Arte.html">Arte</a>
