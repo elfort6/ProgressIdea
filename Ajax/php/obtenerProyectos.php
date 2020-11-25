7d4265733927362692a7cc634ead593cd0bb3cdf
Yefry-yeff <55094597+Yefry-yeff@users.noreply.github.com>
2020-11-24 19:45:07 -0600

    Generacion de rutas mejorada.
    Se mejoro las rutas para el acceso dinamico en el backend.

--- Ajax/php/obtenerProyectos.php
+++ Ajax/php/obtenerProyectos.php
@@ -10,7 +10,7 @@ include 'conexion.php';
         $proyectos .= '<div class="card p-3 pt-4 mt-2 mb-2 tarjeta border border-info">
     <div class="row ">
         <div class="col-md-7">
-            <img src="'.$fila["rutaImagen"].'" class="w-100">
+            <img src="../'.$fila["rutaImagen"].'" class="w-100">
         </div>
         <div class="col-md-5 px-3">
             <div class="card-block px-3 pt-3">
