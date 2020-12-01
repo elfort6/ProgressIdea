996300690796527565771d3a32618f702a5ae5cd
YCM15 <48072372+YCM15@users.noreply.github.com>
2020-11-30 19:24:12 -0600

    coreccion comentario
    correccion de notificacion de comentario

--- Ajax/php/comentario.php
+++ Ajax/php/comentario.php
@@ -5,8 +5,15 @@ $usu=$_SESSION["sesion"]["usuario"];
 $descripcion = $_POST['descripcion'];
 $fecha = $_POST['fecha'];
 $idproyecto=$_POST['idProyecto'];
+
+$consulta1="SELECT Usuario_idUsuario FROM proyecto WHERE idProyecto='".$idproyecto."'";
+$result = $conexion->query($consulta1);
+$fila = $result->fetch_assoc();
+
+$usuarioc = $fila["Usuario_idUsuario"];
+
 $idRandom = rand(0,100000);
-$usuarioc=$_POST['usuarioc'];
+//$usuarioc=$_POST['usuarioc'];
 $consulta5="INSERT INTO `comentario` (`id`, `descripcion`, `fecha`, `usuarioc`,`usuarioactual` ,`idproyecto_proyecto`) values  ('". $idRandom."', '". $descripcion."', '". $fecha ."', '". $usu."','". $usuarioc."','". $idproyecto."') ";
 $result = $conexion->query($consulta5);
 echo("La consulta Ha sido Realiza");
