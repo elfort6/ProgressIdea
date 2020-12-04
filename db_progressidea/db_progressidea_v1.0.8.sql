dbe0236659083388b0ec1fb14ed53cecb95ef2c2
YCM15 <48072372+YCM15@users.noreply.github.com>
2020-12-04 13:47:23 -0600

    Actualizacion de la BDD

--- db_progressidea/db_progressidea_v1.0.8.sql
+++ db_progressidea/db_progressidea_v1.0.8.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 25-11-2020 a las 06:22:06
--- Versión del servidor: 10.4.13-MariaDB
--- Versión de PHP: 7.4.8
+-- Servidor: localhost
+-- Tiempo de generación: 04-12-2020 a las 20:45:42
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `progressidea`
+-- Base de datos: `Progressidea`
 --
 
 -- --------------------------------------------------------
@@ -33,14 +33,6 @@ CREATE TABLE `actualizacionesproyecto` (
   `descripcion` varchar(250) NOT NULL,
   `Proyecto_idProyecto` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `actualizacionesproyecto`
---
-
-INSERT INTO `actualizacionesproyecto` (`idActualizacion`, `titulo`, `descripcion`, `Proyecto_idProyecto`) VALUES
-(3, 'Novedad 1', 'Nueva novedad', 56498),
-(4, 'Comutech', 'KAKAKAKAKAKA', 56498);
 
 -- --------------------------------------------------------
 
@@ -85,6 +77,28 @@ INSERT INTO `categoria` (`idCategoria`, `nombreCategoria`, `descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `id` int(11) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `fecha` varchar(10) NOT NULL,
+  `usuarioc` varchar(50) NOT NULL,
+  `usuarioactual` varchar(50) NOT NULL,
+  `idproyecto_proyecto` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comentario`
+--
+
+INSERT INTO `comentario` (`id`, `descripcion`, `fecha`, `usuarioc`, `usuarioactual`, `idproyecto_proyecto`) VALUES
+(67929, 'Buen proyecto', '4/12/2020', 'kalexd', '38576', 85418);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `correo`
 --
 
@@ -101,9 +115,12 @@ INSERT INTO `correo` (`idCorreo`, `correo`) VALUES
 (17084, 'yeffcode@gmail.com'),
 (26884, 'yefryyo@gmail.com'),
 (38576, 'yu@yu.com'),
+(43490, 'esban@gmail.com'),
+(44796, 'kelvinalexander@gmail.com'),
 (80102, 'yu@yu.com'),
 (90373, 'yeffcode@gmail.com'),
-(91567, 'yunio@yunior.yu');
+(91567, 'yunio@yunior.yu'),
+(99628, 'amlo@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -123,7 +140,8 @@ CREATE TABLE `multimediaproyecto` (
 --
 
 INSERT INTO `multimediaproyecto` (`idImagenesProyecto`, `rutaImagen`, `Proyecto_idProyecto`, `rutaVideo`) VALUES
-(13, 'MultimediaProyectos/yeff/unnamed.jpg', 50558, NULL);
+(15, 'MultimediaProyectos/yuniorcd/comics-anti-venom-wallpaper-preview.jpg', 2385, NULL),
+(16, 'MultimediaProyectos/yuniorcd/product_30565_product_shot_wide.jpg', 85418, NULL);
 
 -- --------------------------------------------------------
 
@@ -140,7 +158,7 @@ CREATE TABLE `persona` (
   `Correo_idCorreo` int(11) NOT NULL,
   `Telefono_idTelefono` int(11) NOT NULL,
   `numId` varchar(45) NOT NULL,
-  `imagen` varchar(45) NOT NULL,
+  `imagen` varchar(45) DEFAULT NULL,
   `direccion` varchar(45) NOT NULL,
   `codigoPostal` varchar(45) NOT NULL,
   `fechaNacimiento` date NOT NULL,
@@ -152,10 +170,11 @@ CREATE TABLE `persona` (
 --
 
 INSERT INTO `persona` (`idRegistro`, `primerNombrel`, `segundoNombre`, `primerApellido`, `segundoApellido`, `Correo_idCorreo`, `Telefono_idTelefono`, `numId`, `imagen`, `direccion`, `codigoPostal`, `fechaNacimiento`, `pais`) VALUES
-(17084, 'Cecilia', 'Luz', 'Rodriguez', 'Lopeteggui', 17084, 17084, '98989898987788', 'img', 'Tegucigalpa', '11110', '2020-10-13', '359'),
-(26884, 'Yefry', 'Luis', 'Ortiz', 'Orellana', 26884, 26884, '0801-1996-15145', 'img', 'Col. San Miguel, calle tocoa casa 5502, Teguc', '504', '2020-10-13', '504'),
-(38576, 'Yunior', 'Marel', 'Cerrato', 'Dominguez', 38576, 38576, '12345', 'img', 'yuiyiu, uasyufias', '1234', '2020-10-13', '504'),
-(90373, 'Yefry', 'Rolando', 'Ortiz', 'Zero', 90373, 90373, '0801199615145', 'img', 'Col. San Miguel, calle tocoa casa 5502, Teguc', '504', '2020-10-13', '504');
+(17084, 'Cecilia', 'Luz', 'Rodriguez', 'Lopeteggui', 17084, 17084, '98989898987788', NULL, 'Tegucigalpa', '11110', '2020-10-13', '359'),
+(26884, 'Yefry', 'Luis', 'Ortiz', 'Orellana', 26884, 26884, '0801-1996-15145', NULL, 'Col. San Miguel, calle tocoa casa 5502, Teguc', '504', '2020-10-13', '504'),
+(38576, 'Yunior', 'Marel', 'Cerrato', 'Dominguez', 38576, 38576, '12345', 'f1.jpg', 'yuniorcerrato26@gmail.com', '1234', '2020-10-13', '504'),
+(90373, 'Yefry', 'Rolando', 'Ortiz', 'Zero', 90373, 90373, '0801199615145', NULL, 'Col. San Miguel, calle tocoa casa 5502, Teguc', '504', '2020-10-13', '504'),
+(99628, 'Andres', 'Manuel', 'Lopez', 'Obrador', 99628, 99628, '1234576', NULL, 'Mexico DF', '456', '2020-10-13', '52');
 
 -- --------------------------------------------------------
 
@@ -176,8 +195,8 @@ CREATE TABLE `proyecto` (
 --
 
 INSERT INTO `proyecto` (`idProyecto`, `nombreproyecto`, `Categoria_idCategoria`, `descripcion`, `Usuario_idUsuario`) VALUES
-(50558, 'Spider-Man  Stories', 5, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the ', 90373),
-(56498, 'Computacion en la casa', 1, 'Que es proyecto de actualizacion', 90373);
+(2385, 'Juegos TGP', 4, 'Lo mejor en videojuegos de todo el mundo', 38576),
+(85418, 'TechnoYo', 1, 'La mejor tecnologia actual en todo', 38576);
 
 -- --------------------------------------------------------
 
@@ -239,9 +258,12 @@ INSERT INTO `telefono` (`idTelefono`, `numeroTelefono`) VALUES
 (17084, '+359 - 435336563'),
 (26884, '31958353'),
 (38576, '+504 - 621546751'),
+(43490, '+504 - 95055281'),
+(44796, '+49 - 90127834'),
 (80102, '+504 - 6752125'),
 (90373, '31958353'),
-(91567, '+504 - 512437');
+(91567, '+504 - 512437'),
+(99628, '+52 - 524621756');
 
 -- --------------------------------------------------------
 
@@ -286,7 +308,8 @@ INSERT INTO `usuario` (`idUsuario`, `usuario`, `contrasenia`, `TipoDeUsuario_idT
 (17084, 'ceci', '$2y$10$uqnGxS6Jy.I9QBkiEZ8HQ.MQjDbTF7LfQaf7nj3zUDihBn7OY0..y', 2, 17084),
 (26884, 'yeff', '$2y$10$8d3ZdymaekYsbFBosyHvp.OibbePK/WpKxMX/N8nxr7DXW/I2mOTG', 1, 26884),
 (38576, 'yuniorcd', '$2y$10$gXPzKiaP5Ay2hLPVafvQneIbXARPIWXOmxD2BcThznHrrMd11zuiO', 1, 38576),
-(90373, 'yeff', '$2y$10$cQFYcnZXRWaxI1mL.N9cfucX0F9l.aoxLWfJdUgZIeqFrz4wu8Knu', 1, 90373);
+(90373, 'yeff', '$2y$10$cQFYcnZXRWaxI1mL.N9cfucX0F9l.aoxLWfJdUgZIeqFrz4wu8Knu', 1, 90373),
+(99628, 'AMLO', '$2y$10$rTvsGq79NdJwKP.13OlWju9OmVk0rO.w6rtWUCfTxMvnXay/GPmCi', 2, 99628);
 
 --
 -- Índices para tablas volcadas
@@ -397,7 +420,7 @@ ALTER TABLE `actualizacionesproyecto`
 -- AUTO_INCREMENT de la tabla `multimediaproyecto`
 --
 ALTER TABLE `multimediaproyecto`
-  MODIFY `idImagenesProyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `idImagenesProyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
