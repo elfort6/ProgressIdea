9014319423db56481e48bda013962336f18c41f5
YCM15 <48072372+YCM15@users.noreply.github.com>
2020-12-04 13:37:57 -0600

    Mas correcciones
    Algunas pequeñas correcciones mas

--- Ajax/php/perfil.php
+++ Ajax/php/perfil.php
@@ -1,25 +1,29 @@
 <?php 
-	include 'conexion.php';
+    include 'conexion.php';
 
-	$usuario = $_POST["usuario"];
+    $usuario = $_POST["usuario"];
 
-	$consulta = "SELECT primerNombrel, segundoNombre, primerApellido, segundoApellido, imagen, u.usuario, c.correo FROM persona p INNER JOIN usuario u ON u.Persona_idRegistro=p.idRegistro INNER JOIN correo c ON c.idCorreo=p.Correo_idCorreo WHERE u.usuario='".$usuario."' LIMIT 1";
+    $consulta = "SELECT primerNombrel, segundoNombre, primerApellido, segundoApellido, imagen, u.usuario, c.correo, TIMESTAMPDIFF(year, p.fechaNacimiento, NOW()) Anios, p.pais FROM persona p INNER JOIN usuario u ON u.Persona_idRegistro=p.idRegistro INNER JOIN correo c ON c.idCorreo=p.Correo_idCorreo WHERE u.usuario='".$usuario."' LIMIT 1";
 
-	$result = $conexion->query($consulta);
+    $result = $conexion->query($consulta);
 
     $fila = $result->fetch_assoc();
 
-    if ($fila["imagen"]!=null) {
-    	$avatar = '<img src="Ajax/'.$fila["imagen"].'">';
+    if ($fila["imagen"]!=null and file_exists("../".$fila["imagen"])) {
+        $avatar = '<img class="avatar" src="Ajax/'.$fila["imagen"].'">';
     }else{
-    	$iniciales = substr($fila["primerNombrel"], 0,1).substr($fila["primerApellido"], 0,1);
-    	$avatar = '<span><b>'.$iniciales.'</b></span>';
+        $iniciales = substr($fila["primerNombrel"], 0,1).substr($fila["primerApellido"], 0,1);
+        $avatar = '<span><b>'.$iniciales.'</b></span>';
     }
 
-    $info = '<div class="my-3 mx-2 avatar">'.$avatar.'</div>
+    $info = '<div class="my-3 mx-3 avatar">'.$avatar.'</div>
             <div class="my-auto">
-	            <p id="user" class="text-dark p-0"><b>'.$fila["primerNombrel"].' '.$fila["segundoNombre"].' '.$fila["primerApellido"].' '.$fila["segundoApellido"].'</b></p>
-	            <p id="correo" class=""><a href="#">'.$fila["correo"].'</a></p>
+                    <p id="user" class="text-dark p-0"><b>'.$fila["primerNombrel"].' '.$fila["segundoNombre"].' '.$fila["primerApellido"].' '.$fila["segundoApellido"].'</b></p>
+                <div class="form-inline">
+                    <p class="text-dark"><b>Edad: '.$fila["Anios"].' Años</b></p>
+                    <p class="text-dark ml-5"><b>Pais: '.$fila["pais"].'</b></p>
+                </div>
+                <p id="correo" class=""><a href="#">'.$fila["correo"].'</a></p>
             </div>';
 
     echo $info;
